bfa0fbaaf66ea95552ba69ba613b9952b6cb18e8
Gabriel.Robot <bieleubolacha@gmail.com>
2025-06-02 14:14:05 -0300

    Delete trabalho/mac.py

--- trabalho/mac.py
+++
@@ -1,8 +0,0 @@
-# Autenticação de Mensagens
-import hmac
-
-chave = b"ciberseguranca eh o melhor curso"
-mensagem = input("(HMAC) > ")
-codigo = hmac.HMAC(chave, mensagem.encode(), "md5")
-
-print(codigo.hexdigest())
